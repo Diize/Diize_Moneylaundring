628a6287259d30803e5da0f908606ab0c196bd9e
Diize <51971449+Diize@users.noreply.github.com>
2021-09-01 19:08:55 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+
+
+
+-----------------------
+--DIIZE_MONEYLAUNDRING
+-----------------------
+-- Config.Location = { x = 1135.740, y = -988.170, z = 46.11 } --Where is the laund located?
+Config.Location = vector3(1135.740, -988.170, 46.11)
+Config.Rate = 0.80 -- How much money in percent should they get back from what they wash? 0.80 = 80%.... aka, the moneywash take a 20% cut.
+Config.MinimumBlackMoney = 100 -- How much should a player have minimum in their inventory to be able to wash?
+Config.MenuKey = 38 -- Change this if youd like, its E by default.
+
+
+------------------------------------------------------------------------------
+------------------------------LOCALES----------------------------------------
+------------------------------------------------------------------------------
+Config.YouGot = "You got $"
+Config.BackFrom = " back from " --make sure you have the spaces like i do here.
+Config.DirtyCash = " dirty cash" 
+Config.NotEnoughDirtyCash = "You dont have enough dirty cash to wash.."
+Config.ProgressbarLabel = "Washing in progress.." --Label for the progressbar?
+Config.ProgressbarTime = 15 -- in seconds, how long should the laundring take?
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+
---
+++ server/server.lua
@@ -0,0 +1,77 @@
+ESX = nil
+stressyo = true
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('checkcash_seventie')
+AddEventHandler('checkcash_seventie', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source) 
+	local myBlackMoney = xPlayer.getInventoryItem(Config.BlackMoney).count
+	local washpercentage_undone = myBlackMoney * 0.70
+	local washpercentage = round(washpercentage_undone)
+	local cleancash = washpercentage * Config.Rate
+	local finalreturn = round(cleancash)
+
+	if myBlackMoney >= Config.MinimumBlackMoney then
+		TriggerClientEvent('animationxd', _source)
+		Wait(Config.ProgressbarTime * 1000)
+		xPlayer.removeAccountMoney('black_money', washpercentage)
+		xPlayer.addMoney(finalreturn)
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.YouGot ..finalreturn.. Config.BackFrom ..washpercentage.. Config.DirtyCash, style = { ['background-color'] = '#ffffff', ['color'] = '#ffffff' } })
+
+	else
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.NotEnoughDirtyCash, style = { ['background-color'] = '#ffffff', ['color'] = '#ffffff' } })
+end
+end)
+
+RegisterNetEvent('checkcash_fifty')
+AddEventHandler('checkcash_fifty', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source) 
+	local myBlackMoney = xPlayer.getInventoryItem(Config.BlackMoney).count
+	local washpercentage_undone = myBlackMoney * 0.50
+	local washpercentage = round(washpercentage_undone)
+	local cleancash = washpercentage * Config.Rate
+	local finalreturn = round(cleancash)
+
+	if myBlackMoney >= Config.MinimumBlackMoney then
+		TriggerClientEvent('animationxd', _source)
+		Wait(Config.ProgressbarTime * 1000)
+		xPlayer.removeAccountMoney('black_money', washpercentage)
+		xPlayer.addMoney(finalreturn)
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.YouGot ..finalreturn.. Config.BackFrom ..washpercentage..Config.DirtyCash, style = { ['background-color'] = '#3d66b4', ['color'] = '#ffffff' } })
+
+	else
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.NotEnoughDirtyCash, style = { ['background-color'] = '#3d66b4', ['color'] = '#ffffff' } })
+end
+end)
+
+RegisterNetEvent('checkcash_hundred')
+AddEventHandler('checkcash_hundred', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source) 
+	local myBlackMoney = xPlayer.getInventoryItem(Config.BlackMoney).count
+	local washpercentage_undone = myBlackMoney * 1
+	local washpercentage = round(washpercentage_undone)
+	local cleancash = washpercentage * Config.Rate
+	local finalreturn = round(cleancash)
+
+	if myBlackMoney >= Config.MinimumBlackMoney then
+		TriggerClientEvent('animationxd', _source)
+		Wait(Config.ProgressbarTime * 1000)
+		xPlayer.removeAccountMoney('black_money', washpercentage)
+		-- xPlayer.addInventoryItem('cash', finalreturn)
+		xPlayer.addMoney(finalreturn)
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.YouGot ..finalreturn.. Config.BackFrom ..washpercentage..Config.DirtyCash, style = { ['background-color'] = '#3d66b4', ['color'] = '#ffffff' } })
+		
+
+	else
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = Config.NotEnoughDirtyCash, style = { ['background-color'] = '3d66b4', ['color'] = '#ffffff' } })
+end
+end)
+
+
+function round(x)
+	return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+  end
